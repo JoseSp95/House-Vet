825045919f940cc8c972617e74a8c4072167d1dd
Jose Suarez <jose95sp@outlook.com>
2017-08-01 04:52:30 -0500

    frmParvovirus terminado

--- src/veterinariaprolog/VeterinariaProlog.java
+++ src/veterinariaprolog/VeterinariaProlog.java
@@ -2,6 +2,7 @@ package veterinariaprolog;
 
 import view.frmLogin;
 import view.frmOtitis;
+import view.frmParvovirus;
 import view.frmRabia;
 import view.frmRabia;
 
@@ -9,7 +10,7 @@ public class VeterinariaProlog {
 
     public static void main(String[] args) {
         //new frmLogin().setVisible(true);
-        new frmOtitis().setVisible(true);
+        new frmParvovirus().setVisible(true);
     }
     
 }
